7fe758e677b8e813a79f7d8eab70f3582fa13c4e
Christina Snyder <christina.snyder11@gmail.com>
2015-02-23 22:20:46 -0500

    writing assembly

--- Assembly/prj2.s
+++ Assembly/prj2.s
@@ -42,7 +42,9 @@ main:           la $sp, stack           ! Initialize stack pointer
                 
                 ! Install timer interrupt handler into vector table
                 noop                    ! FIX ME
-                ei                      ! Don't forget to enable interrupts...
+                la $s0, ti_inthandler
+                sw $s0, 1($zero)
+                ei                      ! Dont forget to enable interrupts...
 
                 la $at, factorial       ! load address of factorial label into $at
                 addi $a0, $zero, 5      ! $a0 = 5, the number to factorialize
@@ -93,6 +95,97 @@ factorial:      addi    $sp, $sp, -1    ! push RA
 
 ti_inthandler:
     noop        ! FIXME
+                addi $sp, $sp, -1
+                sw $k0, 0($sp)
+                ei
+                !save 13 registers (not zero, k0, or sp)
+                addi $sp, $sp, -1
+                sw $at, 0($sp)
+                addi $sp, $sp, -1
+                sw $v0, 0($sp)
+                addi $sp, $sp, -1
+                sw $a0, 0($sp)
+                addi $sp, $sp, -1
+                sw $a1, 0($sp)
+                addi $sp, $sp, -1
+                sw $a2, 0($sp)
+                addi $sp, $sp, -1
+                sw $t0, 0($sp)
+                addi $sp, $sp, -1
+                sw $t1, 0($sp)
+                addi $sp, $sp, -1
+                sw $t2, 0($sp)
+                addi $sp, $sp, -1
+                sw $s0, 0($sp)
+                addi $sp, $sp, -1
+                sw $s1, 0($sp)
+                addi $sp, $sp, -1
+                sw $s2, 0($sp)
+                addi $sp, $sp, -1
+                sw $fp, 0($sp)
+                addi $sp, $sp, -1
+                sw $ra, 0($sp)
+
+                la $s0, seconds
+                lw $a0, 0($s0) !thought about la into $a0, but i need the address again later, so i put it in s0
+                la $s1, minutes ! wont always be necessary to load this, but im going to do it anyways
+                lw $a1, 0($s1)
+                la $s2, hours  !samsies
+                lw $a2, 0($s2)
+
+                addi $a0, $a0, 1 !this aparently happens every second?
+
+                addi $t0, $zero, 60
+                beq $a0, $t0, newMinute
+                beq $zero, $zero, end !under 60 seconds, were done!
+
+    newMinute:  ! weve reached 60 seconds. increment minute and reset seconds
+                addi $a1, $a1, 1
+                add $a0, $zero, $zero
+                beq $a1, $t0, newHour !reached 60 minutes
+                beq $zero, $zero, end !under 60 minutes
+
+    newHour:    addi $a2, $a2, 1
+                add $a1, $zero, $zero
+                !done, so just go to end...
+
+    end:        !put time back in memory
+
+
+                ! restore state
+                lw $ra, 0($sp)
+                addi $sp, $sp, 1
+                lw $fp, 0($sp)
+                addi $sp, $sp, 1
+                lw $s2, 0($sp)
+                addi $sp, $sp, 1
+                lw $s1, 0($sp)
+                addi $sp, $sp, 1
+                lw $s0, 0($sp)
+                addi $sp, $sp, 1
+                lw $t2, 0($sp)
+                addi $sp, $sp, 1
+                lw $t1, 0($sp)
+                addi $sp, $sp, 1
+                lw $t0, 0($sp)
+                addi $sp, $sp, 1
+                lw $a2, 0($sp)
+                addi $sp, $sp, 1
+                lw $a1, 0($sp)
+                addi $sp, $sp, 1
+                lw $a0, 0($sp)
+                addi $sp, $sp, 1
+                lw $v0, 0($sp)
+                addi $sp, $sp, 1
+                lw $at, 0($sp)
+                addi $sp, $sp, 1
+
+                di
+
+                lw $k0, 0($sp)
+                addi $sp, $sp, 1
+
+                reti 
 
 
 
